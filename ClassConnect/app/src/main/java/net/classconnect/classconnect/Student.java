d5beeb81e2f2fa0238e638d944421ca0a4914d72
Gilbert18 <gilbert_maldonado@utexas.edu>
2016-01-16 01:41:48 -0600

    Added fbID and friendsList

--- ClassConnect/app/src/main/java/net/classconnect/classconnect/Student.java
+++ ClassConnect/app/src/main/java/net/classconnect/classconnect/Student.java
@@ -8,11 +8,15 @@ import java.util.List;
 public class Student {
 
     private String name;
+    private String facebookID;
     private List<String> classes;
+    private List<String> friendsList;
 
-    public Student(String name, List<String> classes) {
+    public Student(String facebookID, String name, List<String> classes, List<String> friendsList) {
+        this.facebookID = facebookID;
         this.name = name;
         this.classes = classes;
+        this.friendsList = friendsList;
     }
 
     public String getName() {
