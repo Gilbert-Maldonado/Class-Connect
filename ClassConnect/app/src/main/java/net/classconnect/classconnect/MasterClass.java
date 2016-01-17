b5fb7ced004474b8d42fa9df3b6d00b4754e1524
Gilbert18 <gilbert_maldonado@utexas.edu>
2016-01-17 01:26:34 -0600

    Added stuff

--- ClassConnect/app/src/main/java/net/classconnect/classconnect/MasterClass.java
+++ ClassConnect/app/src/main/java/net/classconnect/classconnect/MasterClass.java
@@ -5,6 +5,7 @@ import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
@@ -17,46 +18,49 @@ public class MasterClass {
     private String name;
     private List<String> friendsList;
     private List<String> courses;
+    private List<String> coursesPlaced;
 
     public MasterClass(String facebookID, String name, List<String> friendsList, List<String> courses) {
         this.facebookID = facebookID;
         this.name = name;
         this.friendsList = friendsList;
         this.courses = courses;
+        for(String current : courses) {
+            coursesPlaced.add(current);
+        }
         addToCourses();
     }
 
     public void addToCourses() {
-
-        // booleans of courses
-        boolean [] coursesPlaced = new boolean[courses.size()];
-        int counter = 0;
         int i = 0;
 
-        while(counter < coursesPlaced.length && i < friendsList.size()) {
+        while(coursesPlaced.size() > 0 && i < friendsList.size()) {
 
             String currentFriend = friendsList.get(i);
 
             ParseQuery<ParseObject> currentQuery = ParseQuery.getQuery("Student");
             currentQuery.whereEqualTo("name", currentFriend);
-            currentQuery.getFirstInBackground(new GetCallback<ParseObject>()
-            {
-                public void done(ParseObject object, ParseException e)
-                {
-                    if (e == null)
-                    {
+            currentQuery.getFirstInBackground(new GetCallback<ParseObject>() {
+                public void done(ParseObject object, ParseException e) {
+                    if (e == null) {
                         List<String> currentList = courses;
                         List<String> otherList = object.getList("courses");
                         currentList.retainAll(new HashSet<String>(otherList));
                         // Classes that we share, add myself to that friend's classes
 
-                        if(currentList.size() != 0) {
-                            counter += currentList.size();
+                        for (int j = 0; j < currentList.size(); j++) {
+                            String currentValue = currentList.get(j);
+                            // Check if the String that is in common is in coursesPlaced (Purpose to avoid repetition)
+                            if (coursesPlaced.contains(currentValue)) {
+                                // Add myself into that class
 
+                                // Then remove from list
+                                coursesPlaced.remove(currentValue);
+
+                            }
                         }
-                    }
-                    else
-                    {
+
+                    } else {
                         //Display that the user does not having matching friends/classes
                         //Do not throw exception or else stack will terminate/App crash
                     }
@@ -68,13 +72,10 @@ public class MasterClass {
         }
 
         // If counter != coursesPlaced, iterate through coursesPlaced array and create groups with the original student in them
-        if(counter < coursesPlaced.length) {
+        if(coursesPlaced.size() > 0) {
             for(i = 0; i < coursesPlaced.length; i++) {
-                if(!coursesPlaced[i]) {
-                    // Then create a new group!
+                // Then create a new group!
 
-
-                }
             }
         }
 
